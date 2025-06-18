1f09417d56e8d6da7175063dadf8d7231bed34bf
Sai-Kiran-Reddy-Thummala <saikiranreddythummala@gmail.com>
2025-06-18 17:24:57 +0530

    added proxy to frontend

--- backend/index.js
+++ backend/index.js
@@ -34,10 +34,10 @@
 
 const express = require('express')
 const app = express()
-const cors = require('cors')
+//const cors = require('cors')
 
 app.use(express.static('dist'))
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
 const requestLogger = (request,response,next) => {
