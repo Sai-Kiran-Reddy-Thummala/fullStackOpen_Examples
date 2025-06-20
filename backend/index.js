92daa4f3b8c9fcf4b2cf3b785c16ea2cdd2b81d7
Sai-Kiran-Reddy-Thummala <saikiranreddythummala@gmail.com>
2025-06-20 20:06:43 +0530

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -81,7 +81,7 @@ app.get('/api/notes',(request,response) => {
   })
 })
 
-app.get('/api/notes/:id',(request,response) => {
+app.get('/api/notes/:id',(request,response,next) => {
     Note.findById(request.params.id).then(note => {
       if(note){
         response.json(note)
@@ -92,7 +92,7 @@ app.get('/api/notes/:id',(request,response) => {
     .catch(error => { next(error) })
 })
 
-app.delete('/api/notes/:id',(request,response) => {
+app.delete('/api/notes/:id',(request,response,next) => {
     Note.findByIdAndDelete(request.params.id).then(result => {
       response.status(204).end()
     })
@@ -107,7 +107,7 @@ const generateId = () => {
     return String(maxId+1)
 }
 
-app.post('/api/notes',(request,response) => {
+app.post('/api/notes',(request,response,next) => {
     
   const body = request.body
 
@@ -122,9 +122,11 @@ app.post('/api/notes',(request,response) => {
     important: body.important || false
   })
 
-  note.save().then(result => {
-      response.json(result)
-  })
+  note.save()
+      .then(result => {
+        response.json(result)
+      })
+      .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -157,6 +159,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).send({error : error.message})
   } 
 
   next(error)
